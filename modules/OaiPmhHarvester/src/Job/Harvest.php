4a63784b247d85978dbf0e9fc003e229a7f38df2
Joris Lambrechts <joris.lambrechts@kuleuven.be>
2025-04-04 15:41:50 +0200

    increase timeout

--- modules/OaiPmhHarvester/src/Job/Harvest.php
+++ modules/OaiPmhHarvester/src/Job/Harvest.php
@@ -164,8 +164,8 @@ class Harvest extends AbstractJob
             }
 
             /** @var \SimpleXMLElement $response */
-            $response = \simplexml_load_file($url);
-            $timeout = 120;
+            //$response = \simplexml_load_file($url);
+            $timeout = 180;
             $context = stream_context_create(['http' => ['timeout' => $timeout]]);
             $data = file_get_contents($url, false, $context);
             $response = simplexml_load_string($data);
